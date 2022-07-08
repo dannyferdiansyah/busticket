24f549ee6a759697b4e34eeb7f80ec034cfe4607
dannyferdiansyah <dannyferdiansyah389@gmail.com>
2022-07-08 18:52:23 +0700

    File TUBES

--- DATABASE FILE/sbtbsphp.sql
+++ DATABASE FILE/sbtbsphp.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 18, 2021 at 06:02 AM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
+-- Generation Time: Jul 08, 2022 at 01:51 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -34,7 +35,7 @@ CREATE TABLE `bookings` (
   `customer_route` varchar(200) NOT NULL,
   `booked_amount` int(100) NOT NULL,
   `booked_seat` varchar(100) NOT NULL,
-  `booking_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `booking_created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -42,14 +43,8 @@ CREATE TABLE `bookings` (
 --
 
 INSERT INTO `bookings` (`id`, `booking_id`, `customer_id`, `route_id`, `customer_route`, `booked_amount`, `booked_seat`, `booking_created`) VALUES
-(60, 'TBZJ360', 'CUST-2114034', 'RT-1908653', 'CITY1 &rarr; CITY2', 100, '3', '2021-10-16 22:15:13'),
-(61, 'QK0MT61', 'CUST-2017936', 'RT-9941455', 'EDROISCHESTER &rarr; BRUGOW', 110, '15', '2021-10-17 22:36:10'),
-(62, 'A8L5662', 'CUST-5585037', 'RT-3835554', 'ZEKA &rarr; ZREGOW', 70, '2', '2021-10-18 00:08:51'),
-(63, 'QDNGC63', 'CUST-8996235', 'RT-3835554', 'ZEKA &rarr; ZREGOW', 70, '15', '2021-10-18 09:31:30'),
-(64, 'X34RW64', 'CUST-9474738', 'RT-3835554', 'ZEKA &rarr; ZREGOW', 70, '6', '2021-10-18 09:32:21'),
-(65, 'JKZVT65', 'CUST-4031139', 'RT-3835554', 'ZEKA &rarr; ZREGOW', 70, '18', '2021-10-18 09:33:36'),
-(66, 'HIIAN66', 'CUST-9997540', 'RT-5887160', 'FLORIA &rarr; ARKBY', 118, '16', '2021-10-18 09:40:16'),
-(67, 'QLOE167', 'CUST-9997540', 'RT-3835554', 'ZEKA &rarr; ZREGOW', 70, '12', '2021-10-18 09:41:01');
+(75, 'JVKX275', 'CUST-6346943', 'RT-2114867', 'CIREBON &rarr; PURBALINGGA', 100, '4', '2022-07-04 11:01:47'),
+(76, '2JLB876', 'CUST-6346943', 'RT-9429366', 'SURABAYA &rarr; JAKARTA', 200, '2', '2022-07-04 11:04:04');
 
 -- --------------------------------------------------------
 
@@ -60,8 +55,8 @@ INSERT INTO `bookings` (`id`, `booking_id`, `customer_id`, `route_id`, `customer
 CREATE TABLE `buses` (
   `id` int(100) NOT NULL,
   `bus_no` varchar(255) NOT NULL,
-  `bus_assigned` tinyint(1) NOT NULL DEFAULT '0',
-  `bus_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `bus_assigned` tinyint(1) NOT NULL DEFAULT 0,
+  `bus_created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -69,17 +64,8 @@ CREATE TABLE `buses` (
 --
 
 INSERT INTO `buses` (`id`, `bus_no`, `bus_assigned`, `bus_created`) VALUES
-(44, 'MVL1000', 0, '2021-10-16 22:05:16'),
-(45, 'ABC0010', 1, '2021-10-17 22:32:46'),
-(46, 'XYZ7890', 0, '2021-10-17 22:33:15'),
-(47, 'BCC9999', 0, '2021-10-17 22:33:22'),
-(48, 'RDH4255', 1, '2021-10-17 22:33:36'),
-(49, 'TTH8888', 1, '2021-10-18 00:05:32'),
-(50, 'MMM9969', 1, '2021-10-18 00:06:02'),
-(51, 'LLL7699', 1, '2021-10-18 00:06:42'),
-(52, 'SSX6633', 0, '2021-10-18 00:06:52'),
-(53, 'NBS4455', 0, '2021-10-18 09:27:49'),
-(54, 'CAS3300', 1, '2021-10-18 09:36:54');
+(61, 'DAN12345', 1, '2022-07-03 16:39:49'),
+(62, 'AKU123', 1, '2022-07-04 11:01:03');
 
 -- --------------------------------------------------------
 
@@ -92,7 +78,7 @@ CREATE TABLE `customers` (
   `customer_id` varchar(255) NOT NULL,
   `customer_name` varchar(30) NOT NULL,
   `customer_phone` varchar(10) NOT NULL,
-  `customer_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `customer_created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -100,13 +86,19 @@ CREATE TABLE `customers` (
 --
 
 INSERT INTO `customers` (`id`, `customer_id`, `customer_name`, `customer_phone`, `customer_created`) VALUES
-(34, 'CUST-2114034', 'Dfirst Dlast', '7002001200', '2021-10-16 22:09:12'),
-(35, 'CUST-8996235', 'Willian Hobbs', '4012222222', '2021-10-17 22:30:23'),
-(36, 'CUST-2017936', 'George Watts', '7011111111', '2021-10-17 22:30:53'),
-(37, 'CUST-5585037', 'Bobb Horn', '1111111110', '2021-10-17 22:31:20'),
-(38, 'CUST-9474738', 'Alan Moore', '7900000000', '2021-10-18 09:32:02'),
-(39, 'CUST-4031139', 'Jamie Rhoades', '1003000010', '2021-10-18 09:33:08'),
-(40, 'CUST-9997540', 'Demo Customer', '7777777700', '2021-10-18 09:39:10');
+(43, 'CUST-6346943', 'Danny Ferdiansyah', '0858548663', '2022-07-03 16:13:34');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `earnings`
+--
+
+CREATE TABLE `earnings` (
+  `id` int(15) NOT NULL,
+  `bus_no` varchar(255) NOT NULL,
+  `booked_amount` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -122,7 +114,7 @@ CREATE TABLE `routes` (
   `route_dep_date` date NOT NULL,
   `route_dep_time` time NOT NULL,
   `route_step_cost` int(100) NOT NULL,
-  `route_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `route_created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -130,14 +122,8 @@ CREATE TABLE `routes` (
 --
 
 INSERT INTO `routes` (`id`, `route_id`, `bus_no`, `route_cities`, `route_dep_date`, `route_dep_time`, `route_step_cost`, `route_created`) VALUES
-(53, 'RT-1908653', 'MVL1000', 'CITY1,CITY2', '2021-10-17', '22:05:00', 100, '2021-10-16 22:05:42'),
-(54, 'RT-3835554', 'MMM9969', 'ZEKA,ZREGOW', '2021-10-19', '23:13:00', 70, '2021-10-16 22:12:32'),
-(55, 'RT-9941455', 'RDH4255', 'EDROISCHESTER,BRUGOW', '2021-10-18', '10:00:00', 110, '2021-10-17 22:34:47'),
-(56, 'RT-9069556', 'XYZ7890', 'ANTALAND,ZREGOW', '2021-10-19', '11:40:00', 85, '2021-10-17 23:39:57'),
-(57, 'RT-775557', 'ABC0010', 'ENCEFORD,VLIRGINIA', '2021-10-19', '13:30:00', 131, '2021-10-17 23:42:12'),
-(58, 'RT-753558', 'TTH8888', 'ARKBY,VEIM', '2021-10-20', '12:04:00', 55, '2021-10-18 00:04:42'),
-(59, 'RT-6028759', 'LLL7699', 'BELRITH,ARKBY', '2021-10-20', '13:50:00', 166, '2021-10-18 00:07:50'),
-(60, 'RT-5887160', 'CAS3300', 'FLORIA,ARKBY', '2021-10-19', '10:30:00', 118, '2021-10-18 09:38:30');
+(66, 'RT-9429366', 'DAN12345', 'SURABAYA,JAKARTA', '2022-07-05', '16:40:00', 200, '2022-07-03 16:40:04'),
+(67, 'RT-2114867', 'AKU123', 'CIREBON,PURBALINGGA', '2022-07-05', '11:01:00', 100, '2022-07-04 11:01:23');
 
 -- --------------------------------------------------------
 
@@ -146,7 +132,8 @@ INSERT INTO `routes` (`id`, `route_id`, `bus_no`, `route_cities`, `route_dep_dat
 --
 
 CREATE TABLE `seats` (
-  `bus_no` varchar(155) NOT NULL,
+  `id` int(15) NOT NULL,
+  `bus_no` varchar(255) NOT NULL,
   `seat_booked` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -154,18 +141,9 @@ CREATE TABLE `seats` (
 -- Dumping data for table `seats`
 --
 
-INSERT INTO `seats` (`bus_no`, `seat_booked`) VALUES
-('ABC0010', NULL),
-('BCC9999', NULL),
-('CAS3300', '16'),
-('LLL7699', NULL),
-('MMM9969', '2,15,6,18,12'),
-('MVL1000', '3'),
-('NBS4455', NULL),
-('RDH4255', '15'),
-('SSX6633', NULL),
-('TTH8888', NULL),
-('XYZ7890', NULL);
+INSERT INTO `seats` (`id`, `bus_no`, `seat_booked`) VALUES
+(4, 'DAN12345', '2'),
+(5, 'AKU123', '4');
 
 -- --------------------------------------------------------
 
@@ -178,7 +156,7 @@ CREATE TABLE `users` (
   `user_fullname` varchar(100) NOT NULL,
   `user_name` varchar(30) NOT NULL,
   `user_password` varchar(255) NOT NULL,
-  `user_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `user_created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -186,7 +164,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `user_fullname`, `user_name`, `user_password`, `user_created`) VALUES
-(1, 'Liam Moore', 'admin', '$2y$10$7rLSvRVyTQORapkDOqmkhetjF6H9lJHngr4hJMSM2lHObJbW5EQh6', '2021-06-02 13:55:21'),
+(1, 'Danny Ferdiansyah', 'admin', '$2y$10$7rLSvRVyTQORapkDOqmkhetjF6H9lJHngr4hJMSM2lHObJbW5EQh6', '2021-06-02 13:55:21'),
 (2, 'Test Admin', 'testadmin', '$2y$10$A2eGOu1K1TSBqMwjrEJZg.lgy.FmCUPl/l5ugcYOXv4qKWkFEwcqS', '2021-10-17 21:10:07');
 
 --
@@ -197,31 +175,47 @@ INSERT INTO `users` (`user_id`, `user_fullname`, `user_name`, `user_password`, `
 -- Indexes for table `bookings`
 --
 ALTER TABLE `bookings`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `customer_id` (`customer_id`),
+  ADD KEY `route_id` (`route_id`),
+  ADD KEY `booked_amount` (`booked_amount`);
 
 --
 -- Indexes for table `buses`
 --
 ALTER TABLE `buses`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `bus_no` (`bus_no`);
 
 --
 -- Indexes for table `customers`
 --
 ALTER TABLE `customers`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `customer_id` (`customer_id`);
+
+--
+-- Indexes for table `earnings`
+--
+ALTER TABLE `earnings`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `bus_no` (`bus_no`),
+  ADD KEY `booked_amount` (`booked_amount`);
 
 --
 -- Indexes for table `routes`
 --
 ALTER TABLE `routes`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `route_id` (`route_id`),
+  ADD KEY `bus_no` (`bus_no`);
 
 --
 -- Indexes for table `seats`
 --
 ALTER TABLE `seats`
-  ADD PRIMARY KEY (`bus_no`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `bus_no` (`bus_no`);
 
 --
 -- Indexes for table `users`
@@ -237,27 +231,75 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `bookings`
 --
 ALTER TABLE `bookings`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
+
 --
 -- AUTO_INCREMENT for table `buses`
 --
 ALTER TABLE `buses`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+
 --
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+
+--
+-- AUTO_INCREMENT for table `earnings`
+--
+ALTER TABLE `earnings`
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT for table `routes`
 --
 ALTER TABLE `routes`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
+
+--
+-- AUTO_INCREMENT for table `seats`
+--
+ALTER TABLE `seats`
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bookings`
+--
+ALTER TABLE `bookings`
+  ADD CONSTRAINT `bookings_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `bookings_ibfk_2` FOREIGN KEY (`route_id`) REFERENCES `routes` (`route_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `earnings`
+--
+ALTER TABLE `earnings`
+  ADD CONSTRAINT `earnings_ibfk_1` FOREIGN KEY (`bus_no`) REFERENCES `buses` (`bus_no`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `earnings_ibfk_2` FOREIGN KEY (`booked_amount`) REFERENCES `bookings` (`booked_amount`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `routes`
+--
+ALTER TABLE `routes`
+  ADD CONSTRAINT `routes_ibfk_1` FOREIGN KEY (`bus_no`) REFERENCES `buses` (`bus_no`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `seats`
+--
+ALTER TABLE `seats`
+  ADD CONSTRAINT `seats_ibfk_1` FOREIGN KEY (`bus_no`) REFERENCES `buses` (`bus_no`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
